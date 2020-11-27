bf3733cad93e52b1b09f033cd8be0cdf74d50c96
journey <tc02117@naver.com>
2020-11-27 11:45:47 +0900

    Step  changed

---
+++ Step12_Interface/src/test/main/MainClass05.java
@@ -0,0 +1,28 @@
+package test.main;
+
+import test.mypac.Drill;
+
+public class MainClass05 {
+	public static void main(String[] args) {
+		useDrill(new Drill() {
+			@Override
+			public void hole() {
+				System.out.println("바닥에 구멍을 3개 뚫어요");
+			}
+		});
+		
+		useDrill(()->{
+			System.out.println("천장에 구멍을 2개 뚫어요");
+		});
+		
+		Drill d1=()->{
+			System.out.println("아무데나 구멍을 뚫어요");
+		};
+		
+		useDrill(d1);
+	}
+	
+	public static void useDrill(Drill d) {
+		d.hole();
+	}
+}
