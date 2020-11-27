0f118070f952da24e90e27b1211653cd84692679
journey <tc02117@naver.com>
2020-11-27 12:16:56 +0900

    Step  added

---
+++ Step13_UtilClass/src/test/main/MainClass01.java
@@ -0,0 +1,22 @@
+package test.main;
+
+import java.util.Scanner;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		/*
+		 * - 키보드로부터 문자열을 입력 받기 위한 Scanner 객체를 생성한다
+		 * - 생성자의 인자로는 키보드로부터 입력 받을 수 있는 
+		 *   InputStream type 객체를 전달해주어야 한다
+		 * - 해당 객체는 System 클래스에 static final 상수 in 이라는 
+		 *   필드에 참조값이 미리 준비 되어 있으므로 그 값을 생성자에 전달하면 된다
+		 */
+		Scanner scan=new Scanner(System.in);
+		// 콘솔창을 여는 용도로 문자열을 한 줄 출력해준다
+		System.out.println("문자열 입력:");
+		// 콘솔로부터 문자열 한 줄 입력 받기
+		String line=scan.nextLine();
+		System.out.println("입력한 내용:"+line);
+		System.out.println("Main 메소드가 종료됩니다");
+	}
+}
