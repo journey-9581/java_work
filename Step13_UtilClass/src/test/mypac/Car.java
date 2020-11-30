82f74ce976d7d57c75138f129cbfea2282f5ebfa
journey <tc02117@naver.com>
2020-11-30 12:19:16 +0900

    Step 13 changed

--- Step13_UtilClass/src/test/mypac/Car.java
+++ Step13_UtilClass/src/test/mypac/Car.java
@@ -5,7 +5,7 @@ public class Car {
 	private String name;
 	// 생성자
 	public Car(String name) {
-		// 생성자의 인다로 전달 받는 차의 이름을 필드에 저장하기
+		// 생성자의 인자로 전달 받는 차의 이름을 필드에 저장하기
 		this.name=name;
 	}
 	// 메소드
