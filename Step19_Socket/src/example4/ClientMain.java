8c5b221aa4334c637ff7d1a72d6145ce45102703
journey <tc02117@naver.com>
2020-12-14 12:21:27 +0900

    Step 19 changed

--- Step19_Socket/src/example4/ClientMain.java
+++ Step19_Socket/src/example4/ClientMain.java
@@ -56,7 +56,7 @@ public class ClientMain extends JFrame implements ActionListener{
 		Socket socket=null;
 		try {
 			// new Socket("접속할 ip 주소", 포트번호)
-			socket=new Socket("127.0.0.1", 5000);
+			socket=new Socket("14.63.164.99", 5000);
 			System.out.println("서버에 Socket 접속 성공!");
 			//2. Socket 을 통해서 출력하기 
 			OutputStream os=socket.getOutputStream();
